6ea817a0327d2b0ea3d8c8a066cc583376396bc5
armenuhi0997 <armenuhimanukyan@annaniks.com>
2021-03-26 16:43:24 +0400

    fixed bugs

--- src/app/pages/main/users/pages/users/users.component.ts
+++ src/app/pages/main/users/pages/users/users.component.ts
@@ -1,14 +1,7 @@
 import { Component } from "@angular/core";
-import { DEFAULT_MASTERS } from "@globals/masters";
-import { DEFAULT_USERS } from "@globals/user";
 import { ServerResponse } from "@models/server-respoce";
 import { IUser } from "@models/users";
-import { Store } from "@ngrx/store";
-import { BreadCrumbState } from "@store/breadcrumb/breadcrumb.state";
-import { SetMasters } from "@store/masters/masters.actions";
-import { selectMasters } from "@store/masters/masters.selectors";
-import { MastersState } from "@store/masters/masters.state";
-import { Observable, Subject } from "rxjs";
+import {  Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 import { UsersService } from "../../users.service";
 
